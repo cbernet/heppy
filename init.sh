f2395ffcb190634a541cf23857d18ba9b5c45c5d
Colin <colin.bernet@cern.ch>
2014-10-10 16:21:01 +0200

    albers now responsible for setting its python environment

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
 export HEPPY=$PWD
 
-echo prepending $ALBERS/examples/python to PYTHONPATH
-export PYTHONPATH=$ALBERS/examples/python:$PYTHONPATH
-
 echo prepending $HEPPY to PYTHONPATH
 export PYTHONPATH=$HEPPY:$PYTHONPATH
 
