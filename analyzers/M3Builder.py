04e59bc66351c07e475dc1d03552622fcf7a275f
clementhelsens <clement.helsens@cern.ch>
2016-03-03 12:21:04 +0100

    modified M3 builder

--- analyzers/M3Builder.py
+++ analyzers/M3Builder.py
@@ -4,12 +4,12 @@ from heppy.particles.tlv.resonance import Resonance
 import pprint 
 import itertools
 
-
 class M3Builder(Analyzer):
     
     def process(self, event):
         jets = getattr(event, self.cfg_ana.jets)
         jets = [jet for jet in jets if self.cfg_ana.filter_func(jet)]
+
         m3 = None
         pt3max=0
         seljets=None
@@ -18,7 +18,6 @@ class M3Builder(Analyzer):
                 pt3=(l[0].p4()+l[1].p4()+l[2].p4()).Pt()
                 if pt3>pt3max:
                     ptmax=pt3
-                    #seljets=[l[0],l[1],l[2]]
                     seljets=l
 
             top_pdgid = 6
