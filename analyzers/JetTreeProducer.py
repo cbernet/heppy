f311269b94430b6441f991f75960f7a7e7169b2a
Gael <gael.touquet@gmail.com>
2016-03-29 11:20:00 +0200

    rec jets attached to gen jets instead of the other way

--- analyzers/JetTreeProducer.py
+++ analyzers/JetTreeProducer.py
@@ -17,9 +17,9 @@ class JetTreeProducer(Analyzer):
         self.tree = Tree( self.cfg_ana.tree_name,
                           self.cfg_ana.tree_title )
         bookJet(self.tree, 'jet1')
-        bookJet(self.tree, 'jet1_gen')
+        bookJet(self.tree, 'jet1_rec')
         bookJet(self.tree, 'jet2')
-        bookJet(self.tree, 'jet2_gen')
+        bookJet(self.tree, 'jet2_rec')
         var(self.tree, 'event')
         var(self.tree, 'lumi')
         var(self.tree, 'run')
@@ -31,6 +31,8 @@ class JetTreeProducer(Analyzer):
             fill(self.tree, 'event', event.eventId)
             fill(self.tree, 'lumi', event.lumi)
             fill(self.tree, 'run', event.run)
+        elif hasattr(event, 'iEv'):
+            fill(self.tree, 'event', event.iEv)
         jets = getattr(event, self.cfg_ana.jets)
         if( len(jets)>0 ):
             jet = jets[0]
@@ -40,14 +42,14 @@ class JetTreeProducer(Analyzer):
                     import pdb; pdb.set_trace()
             fillJet(self.tree, 'jet1', jet)
             if jet.match:
-                fillJet(self.tree, 'jet1_gen', jet.match)
+                fillJet(self.tree, 'jet1_rec', jet.match)
                 # if jet.e()/jet.match.e() > 2.:
                 #     import pdb; pdb.set_trace()
         if( len(jets)>1 ):
             jet = jets[1]
             fillJet(self.tree, 'jet2', jet)
             if jet.match:
-                fillJet(self.tree, 'jet2_gen', jet.match)
+                fillJet(self.tree, 'jet2_rec', jet.match)
         self.tree.tree.Fill()
         
         
