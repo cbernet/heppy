da2d56f3686d1bdb3607467af132d89367c3b2d5
Colin <colin.bernet@cern.ch>
2016-03-30 17:50:34 +0200

    can read lcio events

--- framework/eventslcio.py
+++ framework/eventslcio.py
@@ -2,15 +2,15 @@ from pyLCIO import IOIMPL
 
 class Events(object):
     
-    def __init__(self, filename):
+    def __init__(self, filename, dummy):
         self.reader = IOIMPL.LCFactory.getInstance().createLCReader() 
         self.reader.open(filename)
         
     def __len__(self):
-        return self.reader.getNumberOfEvent()
+        return self.reader.getNumberOfEvents()
 
     def __getattr__(self, key):
         return getattr(self.events, key)
 
-    def __getitem__(self, iEv):
-        pass 
+    def __iter__(self):
+        return iter(self.reader)
