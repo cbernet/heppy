d20e1530f62d7be38341154542527683a4d0573b
clementhelsens <clement.helsens@cern.ch>
2016-04-08 10:40:35 +0200

    modified analyzer

--- test/analysis_hh_ttbar_cfg.py
+++ test/analysis_hh_ttbar_cfg.py
@@ -11,17 +11,13 @@ logging.basicConfig(level=logging.WARNING)
 comp = cfg.Component(
     'example',
     #files = ['example.root']
-    files = ['/afs/cern.ch/user/d/drasal/public/delphes/FCCDelphesOutput_1.root',
-             '/afs/cern.ch/user/d/drasal/public/delphes/FCCDelphesOutput_2.root',
-             '/afs/cern.ch/user/d/drasal/public/delphes/FCCDelphesOutput_3.root',
-             '/afs/cern.ch/user/d/drasal/public/delphes/FCCDelphesOutput_4.root',
-             '/afs/cern.ch/user/d/drasal/public/delphes/FCCDelphesOutput_5.root',
-             '/afs/cern.ch/user/d/drasal/public/delphes/FCCDelphesOutput_6.root',
-             '/afs/cern.ch/user/d/drasal/public/delphes/FCCDelphesOutput_7.root',
-             '/afs/cern.ch/user/d/drasal/public/delphes/FCCDelphesOutput_8.root',
-             '/afs/cern.ch/user/d/drasal/public/delphes/FCCDelphesOutput_9.root',
-             '/afs/cern.ch/user/d/drasal/public/delphes/FCCDelphesOutput_10.root',
-]
+    files = ['root://eospublic.cern.ch//eos/fcc/users/h/helsens/DelphesOutputs/ttbar_13TeV/FCCDelphesOutput_ttbar13TeV_1.root',
+             #'root://eospublic.cern.ch//eos/fcc/users/h/helsens/DelphesOutputs/ttbar_13TeV/FCCDelphesOutput_ttbar13TeV_2.root',
+             #'root://eospublic.cern.ch//eos/fcc/users/h/helsens/DelphesOutputs/ttbar_13TeV/FCCDelphesOutput_ttbar13TeV_3.root',
+             #'root://eospublic.cern.ch//eos/fcc/users/h/helsens/DelphesOutputs/ttbar_13TeV/FCCDelphesOutput_ttbar13TeV_4.root',
+
+        #'/afs/cern.ch/user/h/helsens/FCCsoft/FCCSOFT/FCC/FCCSW/FCCDelphesOutput.root'
+             ]
     #files = ['FCCDelphes_ClementOutput1.root']
 )
 selectedComponents = [comp]
@@ -70,7 +66,7 @@ iso_muons = cfg.Analyzer(
     'sel_iso_muons',
     output = 'sel_iso_muons',
     input_objects = 'muons',
-    filter_func = lambda ptc: ptc.iso.sumpt/ptc.pt()<0.5
+    filter_func = lambda ptc: ptc.iso.sumpt/ptc.pt()<0.2
 )
 
 from heppy.analyzers.Filter import Filter
@@ -88,7 +84,7 @@ iso_electrons = cfg.Analyzer(
     'sel_iso_electrons',
     output = 'sel_iso_electrons',
     input_objects = 'electrons',
-    filter_func = lambda ptc: ptc.iso.sumpt/ptc.pt()<0.5
+    filter_func = lambda ptc: ptc.iso.sumpt/ptc.pt()<0.1
 )
 
 
@@ -121,16 +117,16 @@ from heppy.analyzers.Matcher import Matcher
 match_muon_jets = cfg.Analyzer(
     Matcher,
     delta_r = 0.4,
-    match_particles = 'sel_jets_noelectron_30',
-    particles = 'sel_iso_muons'
+    match_particles = 'sel_iso_muons',
+    particles = 'sel_jets_noelectron_30'
 )
 
-sel_muons_jet = cfg.Analyzer(
+sel_jets_muon = cfg.Analyzer(
     Filter,
     'sel_iso_muons_nojets_30',
-    output = 'sel_iso_muons_nojets_30',
-    input_objects = 'sel_iso_muons',
-    filter_func = lambda muon: muon.match is None
+    output = 'sel_jets_noelectronnomuon_30',
+    input_objects = 'sel_jets_noelectron_30',
+    filter_func = lambda jet: jet.match is None
 )
 
 
@@ -139,7 +135,7 @@ btagging = cfg.Analyzer(
     Btagging,
     'b_jets_30',
     output = 'b_jets_30',
-    input_objects = 'sel_jets_noelectron_30',
+    input_objects = 'sel_jets_noelectronnomuon_30',
     filter_func = lambda jet : jet.tags['bf']>0.
 )
 
@@ -148,8 +144,8 @@ from heppy.analyzers.M3Builder import M3Builder
 m3 = cfg.Analyzer(
     M3Builder,
     instance_label = 'm3',
-    jets = 'sel_jets_noelectron_30', 
-    filter_func = lambda x : x.pt()>35.
+    jets = 'sel_jets_noelectronnomuon_30', 
+    filter_func = lambda x : x.pt()>30.
 )
 
 from heppy.analyzers.MTW import MTW
@@ -172,7 +168,7 @@ selection = cfg.Analyzer(
 from heppy.analyzers.examples.ttbar.TTbarTreeProducer import TTbarTreeProducer
 gen_tree = cfg.Analyzer(
     TTbarTreeProducer,
-    jets_30 = 'sel_jets_noelectron_30',
+    jets_30 = 'sel_jets_noelectronnomuon_30',
     m3 = 'm3',
     met = 'met',
     mtw= 'mtw',
@@ -193,7 +189,7 @@ sequence = cfg.Sequence( [
     match_jet_electrons,
     sel_jets_electron,
     match_muon_jets,
-    sel_muons_jet,
+    sel_jets_muon,
     btagging,
     selection,
     m3, 
