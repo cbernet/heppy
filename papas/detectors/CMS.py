4f7551e8e9c4c2c6ab9573f5fd2ff4bc6a153da2
Gael <gael.touquet@gmail.com>
2016-05-17 15:06:07 +0200

    Tuned CMS

--- papas/detectors/CMS.py
+++ papas/detectors/CMS.py
@@ -1,6 +1,6 @@
-from detector import Detector, DetectorElement
-import material
-from geometry import VolumeCylinder
+from heppy.papas.detectors.detector import Detector, DetectorElement
+import heppy.papas.detectors.material as material
+from heppy.papas.detectors.geometry import VolumeCylinder
 import math
 import random
 
@@ -8,17 +8,23 @@ class ECAL(DetectorElement):
 
     def __init__(self):
         volume = VolumeCylinder('ecal', 1.55, 2.1, 1.30, 2. )
-        mat = material.Material('ECAL', 8.9e-3, 0.275)
+        mat = material.Material('ECAL', 18.062e-3, 0.275)#0.240 test gael
         self.eta_crack = 1.5
-        self.emin = 0.4
-        self.eres = [0.073, 0.1, 0.005]
+        self.emin = 0.3
+        self.eres = {'barrel':[0.073, 0.1, 0.005], 'endcap':[0.213, 0.224, 0.005]}
         super(ECAL, self).__init__('ecal', volume,  mat)
-        
-    def energy_resolution(self, energy, theta=0.):
-        stoch = self.eres[0] / math.sqrt(energy)
-        noise = self.eres[1] / energy
-        constant = self.eres[2]
+
+    def energy_resolution(self, energy, eta=0.):
+        part = 'barrel'
+        if abs(eta)>1.479 and abs(eta)<3.0:
+            part = 'endcap'
+        stoch = self.eres[part][0] / math.sqrt(energy)
+        noise = self.eres[part][1] / energy
+        constant = self.eres[part][2]
         return math.sqrt( stoch**2 + noise**2 + constant**2) 
+
+    def energy_response(self, energy, theta=0):
+        return 1.
 
     def cluster_size(self, ptc):
         pdgid = abs(ptc.pdgid())
@@ -32,25 +38,32 @@ class ECAL(DetectorElement):
         eta = abs(cluster.position.Eta())
         if eta < self.eta_crack:
             return energy>self.emin
-        elif eta < 3.:
-            return energy>self.emin and cluster.pt>0.5
+        elif eta < 2.93:
+            return energy>1 and cluster.pt>0.2
         else:
             return False
 
     def space_resolution(self, ptc):
         pass
-
     
 class HCAL(DetectorElement):
 
     def __init__(self):
         volume = VolumeCylinder('hcal', 2.9, 3.6, 1.9, 2.6 )
         mat = material.Material('HCAL', None, 0.17)
-        self.eres = [1.1, 0., 0.]
+        self.eres = [1.25829, 0., 0.175950]
+        self.eresp = [1.03430, 5.23646, -2.03400]
+        #self.eresp = [1.03845, 3.80811, 2.50753]
         super(HCAL, self).__init__('ecal', volume, mat)
 
     def energy_resolution(self, energy, theta=0.):
-        return self.eres[0]/ math.sqrt( energy ) 
+        stoch = self.eres[0]/ math.sqrt( energy ) 
+        constant = self.eres[2]
+        return math.sqrt( stoch**2 + constant**2)
+
+    def energy_response(self, energy, theta=0):#test gael
+        return self.eresp[0]/(1+math.exp((energy-self.eresp[1])/self.eresp[2]))
+        #return self.eresp[0]*(1-math.exp(-(energy-self.eresp[1])/self.eresp[2]))
 
     def cluster_size(self, ptc):
         return 0.2
@@ -58,7 +71,12 @@ class HCAL(DetectorElement):
     def acceptance(self, cluster):
         energy = cluster.energy
         eta = abs(cluster.position.Eta())
-        if eta < 3. : 
+        if eta < 1.3 :
+            if energy>1:
+                return random.uniform(0,1)<(1/(1+math.exp((energy-1.93816)/(-1.75330))))
+            else:
+                return False
+        elif eta < 3. : 
             return energy>4.
         elif eta < 5.:
             return energy>7.
@@ -75,6 +93,7 @@ class Tracker(DetectorElement):
     
     def __init__(self):
         volume = VolumeCylinder('tracker', 1.29, 1.99)
+        #gael test
         mat = material.void
         super(Tracker, self).__init__('tracker', volume,  mat)
 
@@ -82,8 +101,10 @@ class Tracker(DetectorElement):
         # return False
         pt = track.pt
         eta = abs(track.p3.Eta())
-        if eta < 2.5 and pt>0.5:
-            return random.uniform(0,1)<1. # CMS without tracker material effects 
+        if eta < 1.35 and pt>0.5:
+            return random.uniform(0,1)<0.95
+        elif eta < 2.5 and pt>0.5:
+            return random.uniform(0,1)<0.9 
         else:
             return False
 
