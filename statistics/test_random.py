97db28c3fff4974f6efe03a66e4dea63ce719a95
Alice <alice.robson@cern.ch>
2016-09-20 12:49:35 +0200

    new test_random.py

---
+++ statistics/test_random.py
@@ -0,0 +1,45 @@
+import unittest
+import logging
+logging.getLogger().setLevel(logging.ERROR)
+import random
+
+class TestRandom(unittest.TestCase):
+
+# setting the random seed for reproducible results
+
+
+    def test_seed(self):
+
+        #unseeded
+        r0 = random.uniform(0, 1)
+        r1 = random.expovariate(3000)
+        r2 = random.gauss(1,3)    
+     
+        #seed
+        random.seed(0xdeadbeef)
+        a0 = random.uniform(0, 1)
+        a1 = random.expovariate(3)
+        a2 = random.gauss(1,3)
+        
+        #reseed
+        random.seed(0xdeadbeef)
+        b0 = random.uniform(0, 1)
+        b1 = random.expovariate(3)
+        b2 = random.gauss(1,3)
+    
+        #unseeded should be different to seeded
+        self.assertFalse(a0==r0)
+        self.assertFalse(a1==r1)
+        self.assertFalse(a2==r2)        
+        
+        #reseeded should be same as seeded
+        self.assertEqual(a0,b0)
+        self.assertEqual(a1,b1)
+        self.assertEqual(a2,b2)
+    
+        pass
+    
+    
+if __name__ == '__main__':
+
+    unittest.main()
