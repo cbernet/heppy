487b5e06f8d204143a92481c85b160a476b986e9
Alice <alice.robson@cern.ch>
2016-11-09 13:44:23 +0100

    increase documentation

--- display/core.py
+++ display/core.py
@@ -1,25 +1,27 @@
-from ROOT import TCanvas, TH1, TH2F
+'''Event Display class classes'''
 import operator
 import math
 import os
 
-
-'''Display class
-Main class to produce ROOT plots of the detector and display clusters, trajectories etc
-
-Example usage
-    self.display = Display(["xy", "yz"], subscreens=["simulated", "reconstructed"])
-    self.display.register(self.gdetector, layer=0, clearable=False)
-    ... (add clusters tracks lines etc)
-    self.display.draw()
-
-'''
-
 class Display(object):
-
+    '''
+    Main class to produce Event Display windows/plots of the detector and 
+    clusters, trajectories etc.
+    Separate windows (ViewPane) are shown for the different projections.
+    Subscreens (ViewPad) may be used within windows, for exampe to compare 
+    simulated and recontructed particles.
+    
+    Example usage
+        self.display = Display(["xy", "yz"], subscreens=["simulated", "reconstructed"])
+        self.display.register(self.gdetector, layer=0, clearable=False)
+        ... (add clusters tracks lines etc)
+        self.display.draw()
+    
+    '''
     def __init__(self, projections=None, subscreens=None):
-        '''projections: list of projections eg ['xy', 'yz', 'xz'] these will be separate windows
-           subscreens: list of names of panels within each window eg subscreens=["simulated", "reconstructed"]
+        '''@param projections: list of projections eg ['xy', 'yz', 'xz' ,'ECAL_thetaphi', 'HCAL_thetaphi']
+                 these will be separate windows
+           @param subscreens: list of names of panels within each window eg subscreens=["simulated", "reconstructed"]
                  each of the projection windows will contain all of the subscreens
                  the subscreens can be used to show different aspects of an event, eg simulated particles and reconstructed particles
         '''
@@ -41,8 +43,13 @@ class Display(object):
                                             500, 1000, subscreens=subscreens)
 
     def register(self, obj, layer, clearable=True, sides=None):
-        '''add in an object such as a cluster or trajectory
-           the object will be registered in every projection and subscreen
+        '''object will be registered in every projection and in selected subscreen
+           @param obj: thing to be plotted, this will have a draw function. Often will be
+                       a ROOT type object such as TGraph, TEllipse etc
+           @param layer: what layer in plot to use. Higher layers are plotted **on top- check**
+           @param clearable: not sure
+           @param sides: if None wil send to all subscreens, otherwise side=0 is first/left subscreen
+                         side = 1 is second (right) subscreen
         '''
         elems = [obj]
         if hasattr(obj, '__iter__'):
@@ -58,6 +65,10 @@ class Display(object):
     def zoom(self, xmin, xmax, ymin, ymax):
         '''intended to be called from the command line
             all projections and subscreens are zoomed
+        @param xmin: lower left corner x for the zoomed in screen using hist coordinates
+        @param xmax: upper right corner x for the zoomed in screen using hist coordinates
+        @param ymin: lower left corner y for the zoomed in screen using hist coordinates
+        @param ymax: upper right corner y for the zoomed in screen using hist coordinates
         '''
         for view in self.views.values():
             view.zoom(xmin, xmax, ymin, ymax)
@@ -75,8 +86,13 @@ class Display(object):
         for view in self.views.values():
             view.draw()
 
-    def save(self, outdir, filename = '', filetype='png'):
-        '''write the plot to a png file (one file per projection)'''
+    def save(self, outdir, filename='', filetype='png'):
+        '''write the plot to file (one file per projection)
+        @param outdir: directory for output files
+        @param filename: base of the final filename eg "event_0" would end up with outputs
+                         such as "****xy_event_0_simulation****""
+        @param filetype: eg png file output. ***what else is supported***
+        '''
         if not os.path.exists(outdir):        
             os.mkdir(outdir) 
         for view in self.views.values():
@@ -87,10 +103,18 @@ class ViewPane(object): #a graphics window
     def __init__(self, name, projection, nx, xmin, xmax, ny, ymin, ymax,
                  dx=600, dy=600, subscreens=None):
         '''arguments
-        projection = name of the projection (string)
-        nx, xmin, xmax, ny,ymin, ymax = the axis specification for the ROOT hists
-        dx dy = size of each subscreen (pixels?)
-        subscreens = list of names of the subscreens
+        @name  name = title name for the pane
+        @param projection = name of the projection (string)
+                            eg one of 'xy', 'yz', 'xz' ,'ECAL_thetaphi', 'HCAL_thetaphi'
+        @param nx: points on x axis
+        @param xmin: lower left corner x for ROOT hists
+        @param xmax: upper right corner x for ROOT hists
+        @param nx: points on x axis
+        @param ymin: lower left corner y for ROOT hists
+        @param ymax: upper right corner y for ROOT hists
+        @param dx: horizontal size of each subscreen in pixels
+        @param dy: vertical size of each subscreen in pixels
+        @param subscreens: list of names of the subscreens
         '''
         self.projection = projection
         self.hists = dict()
@@ -100,29 +124,18 @@ class ViewPane(object): #a graphics window
         ty = 50
         width = 1
         height = 1
-        self.nsubscreens = 1
-        self.subscreens = dict()
- 
+       
+        
         #decide how to subdivide the window for the subscreens       
         #normally split screen into horizontally side by side
+        self.subscreens = dict()
         if subscreens is None:
             #The projection name and the subscreen name get concatenated. eg "xy: simulated",
             #when there is only one subscreen the subscreen string is not needed so is set to ""             
             subscreens = [""]
         self.nsubscreens = len(subscreens)
         width = width * self.nsubscreens # normal case
-        #experimental case for large number of small subscreens
-        if self.nsubscreens%8 == 0 and self.nsubscreens >= 8: 
-            # make the sceen be 4 subscreens wide, and halve the size of each subscreen                
-            width = width*4
-            height = height *self.nsubscreens/4
-            dx = dx/2
-            dy = dy/2
-            xmax = 2
-            ymin = -xmax
-            xmin = -xmax
-            ymax = xmax
-
+        
         self.canvas = TCanvas(name, name, tx, ty, width *dx, height*dy)
         self.canvas.SetRightMargin(0.)
         self.canvas.SetLeftMargin(0.)
@@ -146,7 +159,14 @@ class ViewPane(object): #a graphics window
         self.__class__.nviews += 1 
         
     def register(self, obj, layer, clearable=True, sides=None):
-        #register this object on all the subscreens for this projection
+        ''' the object will be registered in selected subscreens
+            @param obj: thing to be plotted, this will have a draw function. Often will be
+                        a ROOT type object such as TGraph, TEllipse etc
+            @param layer: what layer in plot to use. Higher layers are plotted **on top- check**
+            @param clearable: not sure
+            @param sides: if None will send to all subscreens, otherwise side=0 is first/left subscreen
+                          side = 1 is second (right) subscreen
+        '''        
         if sides is None:
             sides = range(0, len(self.subscreens))
         for x in sides:
@@ -157,7 +177,8 @@ class ViewPane(object): #a graphics window
             p.clear()
         
     def draw(self):
-        #draw all the subcreens for this projection window
+        '''draw all subscreens
+        '''
         for x in range(0, self.nsubscreens):
             if self.canvas is None:
                 pass
@@ -167,27 +188,54 @@ class ViewPane(object): #a graphics window
                 self.canvas.Update()
 
     def zoom(self, xmin, xmax, ymin, ymax):
-        #all the subscreens are zoomed at once
+        '''intended to be called from the command line
+            all subscreens are zoomed
+        @param xmin: lower left corner x for the zoomed in screen using hist coordinates
+        @param xmax: upper right corner x for the zoomed in screen using hist coordinates
+        @param ymin: lower left corner y for the zoomed in screen using hist coordinates
+        @param ymax: upper right corner y for the zoomed in screen using hist coordinates
+        '''
         for p in self.subscreens.itervalues():
             p.zoom(xmin, xmax, ymin, ymax)
         self.draw()    
 
     def unzoom(self):
+        '''intended to be called from the command line
+            all subscreens are unzoomed
+         '''          
         for p in self.subscreens.itervalues():
             p.unzoom()
         self.draw()   
 
     def save(self, outdir, filename, filetype):
+        '''write the screen to file 
+            @param outdir: directory for output files
+            @param filename: base of the final filename eg "event_0" would end up with outputs
+                             such as "****xy_event_0_simulation****""
+            @param filetype: eg png file output. ***what else is supported***
+            '''        
         fname = '{outdir}/{name}.{filetype}'.format(outdir=outdir,
                                                     name=self.canvas.GetName() + filename,
                                                     filetype=filetype)
         self.canvas.SaveAs(fname)
 
   
-class ViewPad(object):  #a graphics subscreen (within a window)
-    
+class ViewPad(object):  
+    '''ViewPad is a graphics subscreen, ie a subscreen within a projection window
+    '''   
     def __init__(self, name, projection, nx, xmin, xmax, ny, ymin, ymax, side=0):
-        #side is the index saying which subscreen we are plotting to
+        '''arguments
+        @name  name = title name for the pane
+        @param projection = name of the projection (string)
+                            eg one of 'xy', 'yz', 'xz' ,'ECAL_thetaphi', 'HCAL_thetaphi'
+        @param nx: points on x axis
+        @param xmin: lower left corner x for ROOT hists
+        @param xmax: upper right corner x for ROOT hists
+        @param nx: points on x axis
+        @param ymin: lower left corner y for ROOT hists
+        @param ymax: upper right corner y for ROOT hists
+        @param side: documents which subscreen we are in 0 = main or left, 1 = right
+        '''        
         self.side = side
         self.projection = projection
         TH1.AddDirectory(False)
@@ -199,7 +247,12 @@ class ViewPad(object):  #a graphics subscreen (within a window)
         self.locked = dict()
  
     def register(self, obj, layer, clearable=True):
-        #adds an object that will be plotted
+        ''' registers an object on a subscreen
+            @param obj: thing to be plotted, this will have a draw function. Often will be
+                        a ROOT type object such as TGraph, TEllipse etc
+            @param layer: what layer in plot to use. Higher layers are plotted **on top- check**
+            @param clearable: not sure
+        '''    
         self.registered[obj] = layer
         if not clearable:
             self.locked[obj] = layer
@@ -208,18 +261,27 @@ class ViewPad(object):  #a graphics subscreen (within a window)
         self.registered = dict(self.locked.items())
         
     def draw(self):
-        #draw everything in this subscreen
+        '''draw everything in this subscreen
+        '''
         for obj, layer in sorted(self.registered.items(),
                                  key=operator.itemgetter(1)):
             obj.draw(self.projection)
 
     def zoom(self, xmin, xmax, ymin, ymax):
-        #zoom a subscreen
+        '''intended to be called from the command line
+            subscreen is zoomed
+        @param xmin: lower left corner x for the zoomed in screen using hist coordinates
+        @param xmax: upper right corner x for the zoomed in screen using hist coordinates
+        @param ymin: lower left corner y for the zoomed in screen using hist coordinates
+        @param ymax: upper right corner y for the zoomed in screen using hist coordinates
+        '''
         self.hist.GetXaxis().SetRangeUser(xmin, xmax)
         self.hist.GetYaxis().SetRangeUser(ymin, ymax)
 
     def unzoom(self):
-        #unzoom a subscreen
+        '''intended to be called from the command line
+           subscreen is unzoomed
+        '''  
         self.hist.GetXaxis().UnZoom()
         self.hist.GetYaxis().UnZoom()
         
