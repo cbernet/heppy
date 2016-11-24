56b364f4db8b8780bc16df28e34bcde7024a9834
Colin <colin.bernet@cern.ch>
2016-11-24 08:51:31 +0100

    random seed and number of events managed properly

--- scripts/pythia_batch.py
+++ scripts/pythia_batch.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import re
 from heppy.utils.batchmanager import BatchManager
 
 
@@ -32,16 +33,39 @@ class MyBatchManager( BatchManager ):
                                                self.cardfname) )
         scriptFile.close()
         os.system('chmod +x %s' % scriptFileName)
-        shutil.copy(self.cardfname, jobDir)
-
+        configfile = open('/'.join([jobDir, self.cardfname]), 'w')
+        configfile.writelines(self.config)
+        configfile.close()
+        
+def build_config(fname, nevents):
+    '''make sure that pythia config file has the correct seeding
+    parameters, and set the number of events.
+    '''
+    nevents_pattern = re.compile('\s*Main:numberOfEvents\s*=\s*\d+.*')
+    ifile = open(fname)
+    config = ifile.readlines()
+    newconfig = []
+    for line in config:
+        if nevents_pattern.match(line):
+            line = 'Main:numberOfEvents = {}\n'.format(nevents) 
+        newconfig.append(line)
+    newconfig.extend([  
+        'Random:setSeed = on\n',
+        'Random:seed = 0\n'
+    ])   
+    ifile.close()
+    return newconfig
 
 def main(options, args, batchManager):
-    if len(args) != 2:
+    if len(args) != 3:
         batchManager.Parser().error('incorrect number of arguments')
-    cardfname, njobs = args
+    cardfname, njobs, nevents = args
     njobs = int(njobs)
+    nevents = int(nevents)
+    print cardfname, njobs, nevents
+    
+    batchManager.config = build_config(cardfname, nevents)
     batchManager.cardfname = cardfname
-    print cardfname, njobs
     
     listOfValues = range(njobs)
     batchManager.PrepareJobs( listOfValues )
@@ -52,7 +76,7 @@ def main(options, args, batchManager):
 if __name__ == '__main__':
     batchManager = MyBatchManager()
     batchManager.parser_.usage="""
-    %prog [options] <pythia_card_file> <njobs>
+    %prog [options] <pythia_card_file> <njobs> <nevents>
 
     Run fcc-pythia8-generate on the batch. 
     """
