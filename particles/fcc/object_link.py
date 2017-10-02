b6779a32937979ea85c7af18d9550b93f8562fd6
Alice <alice.robson@cern.ch>
2017-10-02 18:02:51 +0200

    add objectlink

---
+++ particles/fcc/object_link.py
@@ -0,0 +1,17 @@
+from heppy.particles.object_link import ObjectLink as BaseLink
+from pod import POD
+from heppy.papas.data.identifier import Identifier
+
+from ROOT import TLorentzVector
+from ROOT import TVector3
+
+class ObjectLink(BaseLink, POD):
+
+    def __init__(self, fccobj, parenttype='g', childtype='r'):
+        super(ObjectLink, self).__init__(fccobj)
+        self.incoming = []
+        self.outgoing = []
+        self._id1 =  (fccobj.sim().getObjectID().index,
+                       fccobj.sim().getObjectID().collectionID) 
+        self._id2 =  (fccobj.rec().getObjectID().index,
+                      fccobj.rec().getObjectID().collectionID)         
