9f2ded776b6dc460b1d5b00dac969984f5280560
Colin <colin.bernet@cern.ch>
2017-10-20 13:35:20 +0200

    JetEnergyCorrector can now modify jets in place if output not specified (deepcopy expensive!)

--- analyzers/JetEnergyCorrector.py
+++ analyzers/JetEnergyCorrector.py
@@ -15,13 +15,16 @@ class JetEnergyCorrector(Analyzer):
             ParametrizedBTagger,
             output = 'jets_corr',
             input_jets = 'jets',
-            detector = CMS
+            detector = CMS,
         )
   
     @param input_jets: input collection of jets. 
     @param detector: L{detector, like CMS<heppy.papas.detectors.CMS.CMS>}.
       The detector has the method jet_energy_correction(jet), returning
       the scaling factor.
+    @param output: optional. if present, jets are deepcopied before correction,
+      and a new collection of jets is added to the event. If not present,
+      jets are modified in place.
       
     The 4-momentum of the calibrated jet is obtained by scaling
     the 4-momentum of the input jet. 
@@ -36,10 +39,18 @@ class JetEnergyCorrector(Analyzer):
         containing a copy of each input jet, with a corrected energy
         '''
         jets = getattr(event, self.cfg_ana.input_jets)
-        jets_rescaled = []
+        output = None
+        if hasattr(self.cfg_ana, 'output'):
+            output = []
         for jet in jets:
             scaling_factor = self.cfg_ana.detector.jet_energy_correction(jet)
-            jet_rescaled = copy.deepcopy(jet)
-            jet_rescaled._tlv *= scaling_factor
-            jets_rescaled.append(jet_rescaled)
-        setattr(event, self.cfg_ana.output, jets_rescaled)
+            if output is not None:
+                outjet = copy.deepcopy(jet)
+                outjet._tlv *= scaling_factor
+                output.append(outjet)
+            else:
+                jet._tlv *= scaling_factor
+        if output:
+            setattr(event, self.cfg_ana.output, output)
+            
+            
