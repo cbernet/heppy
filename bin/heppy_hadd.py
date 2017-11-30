c868fd4bb4f174a6b1b904bcfcbbf5341176694d
Colin <colin.bernet@cern.ch>
2017-11-30 11:21:51 +0100

    bug fix in heppy_hadd: can now add all pickle files

--- bin/heppy_hadd.py
+++ bin/heppy_hadd.py
@@ -21,8 +21,11 @@ def haddPck(file, odir, idirs):
     basedir = os.getcwd()
     fileName = os.path.basename(file)
     for dirpath in idirs:
-        # fileName = file.replace( idirs[0], dirpath )
-        os.chdir(dirpath)
+        fileName = file.replace( idirs[0], dirpath )
+        # os.chdir(dirpath)
+        # import pdb; pdb.set_trace()
+        # print dirpath
+        # print fileName
         pckfile = open(fileName)
         sys.path.insert(0, dirpath)
         obj = pickle.load(pckfile)
