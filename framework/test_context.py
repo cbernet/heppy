38152634f05971d8ab3a3f4c6fb68fb56606723c
Colin Bernet <colin.bernet@cern.ch>
2019-02-04 14:29:08 +0100

    deal with cases where FCC and CMS contexts are not set (root and bare)

--- framework/test_context.py
+++ framework/test_context.py
@@ -18,6 +18,14 @@ class TestContext(unittest.TestCase):
         env = dict(PODIO='.', FCCEDM='.', FCCPHYSICS='.', HEPPY='.',
                    CMSSW_BASE='.')        
         self.assertRaises( ValueError, get_name, env )
+     
+    def test_root(self):
+        env = dict(ROOTSYS='.')
+        self.assertEqual(get_name(env), 'root')
+
+    def test_bare(self):
+        env = dict()
+        self.assertEqual(get_name(env), 'bare')
         
 
 if __name__ == '__main__':
